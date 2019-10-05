dd89a6aea0ce847e90654b2fedbc9a68cfa013df
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-05 18:23:38 -0300

    Changed signUp url: users to user.

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -27,7 +27,7 @@ export function* signUp({ payload }) {
   try {
     const { name, email, password } = payload;
 
-    yield call(api.post, 'users', {
+    yield call(api.post, 'user', {
       name,
       email,
       password,
