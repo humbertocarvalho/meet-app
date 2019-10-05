018eb959196a851470a927402b1804dcf4fd4a32
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-05 18:27:18 -0300

    Add dashboardpage.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,15 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      {/* <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/profile" component={Profile} isPrivate /> */}
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      {/* <Route path="/profile" component={Profile} isPrivate /> */}
     </Switch>
   );
 }
