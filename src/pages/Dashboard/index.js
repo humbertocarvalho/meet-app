6cb7fa426c60932ec98f6fa15ddf3a0a1fa0ffd9
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-06 21:24:25 -0300

    Created Meetups list.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,16 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdAddCircleOutline } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Meetups, Meetup } from './styles';
 
 export default function Dashboard() {
   return (
     <Container>
-      <h1>Meus meetups</h1>
-      <Link to="meetups/create">
-        <MdAddCircleOutline color="#fff" size={22} />
-        Novo Meetup
-      </Link>
+      <header>
+        <h1>Meus meetups</h1>
+        <Link to="meetups/create">
+          <MdAddCircleOutline color="#fff" size={22} />
+          Novo Meetup
+        </Link>
+      </header>
+
+      <Meetups>
+        <Meetup>
+          <span>Meetup de React Native</span>
+          <Link to="meetups/create">
+            <MdAddCircleOutline color="#fff" size={22} />
+          </Link>
+        </Meetup>
+        <Meetup>
+          <span>Meetup de React Native</span>
+          <Link to="meetups/create">
+            <MdAddCircleOutline color="#fff" size={22} />
+          </Link>
+        </Meetup>
+        <Meetup>
+          <span>Meetup de React Native</span>
+          <Link to="meetups/create">
+            <MdAddCircleOutline color="#fff" size={22} />
+          </Link>
+        </Meetup>
+        <Meetup>
+          <span>Meetup de React Native</span>
+          <Link to="meetups/create">
+            <MdAddCircleOutline color="#fff" size={22} />
+          </Link>
+        </Meetup>
+      </Meetups>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,27 +3,51 @@ import styled from 'styled-components';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
+  header {
+    display: flex;
+    justify-content: space-between;
 
+    h1 {
+      color: #fff;
+    }
+
+    a {
+      background: #f94d6a;
+      border-radius: 4px;
+      padding: 10px 14px;
+      display: flex;
+      align-items: center;
+      font-weight: bold;
+      font-size: 15px;
+      color: #fff;
+      transition: background 0.3s;
+    }
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+export const Meetups = styled.ul`
+  margin-top: 60px;
+  list-style: none;
+  background: #22202c;
+  width: 600px;
+  height: 62px;
+  border-radius: 4px;
+`;
+
+export const Meetup = styled.li`
   display: flex;
   justify-content: space-between;
 
-  h1 {
-    color: #fff;
-  }
-
-  a {
-    background: #f94d6a;
-    border-radius: 4px;
-    padding: 10px 14px;
-    display: flex;
+  span {
     align-items: center;
-    font-weight: bold;
-    font-size: 15px;
     color: #fff;
-    transition: background 0.3s;
+    font-size: 18px;
+    font-weight: bold;
   }
 
-  svg {
-    margin-right: 5px;
-  }
+  margin-bottom: 10px;
 `;
