e6d85edea3435984ac0f19ee724f89194fb60795
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-08 07:06:35 -0300

    Created Profile page.

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { MdAddCircleOutline } from 'react-icons/md';
+
+import { Form, Input } from '@rocketseat/unform';
+import { useDispatch, useSelector } from 'react-redux';
+
+// import { updateProfileRequest } from '~/store/modules/user/actions';
+// import { signOut } from '~/store/modules/auth/actions';
+import { Container } from './styles';
+
+export default function Profile() {
+  const dispatchEvent = useDispatch();
+  // const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  function handleSignOut() {}
+
+  return (
+    <Container>
+      <Form
+        initialData={{ name: 'Humberto', email: 'hvcarvalhobrt@gmail.com' }}
+        onSubmit={handleSubmit}
+      >
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">
+          <MdAddCircleOutline color="#fff" size={22} />
+          Salvar perfil
+        </button>
+      </Form>
+    </Container>
+  );
+}
