f30324974590e50356a6ced5d016c0ea4e55fddf
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-08 16:14:38 -0300

    Stylized meetup page.

--- src/pages/Meetup/index.js
+++ src/pages/Meetup/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { MdAddCircleOutline } from 'react-icons/md';
+
+import { Container, Botao } from './styles';
 
 export default function Meetup() {
-  return <h1>Aqui é o create caralho vuai</h1>;
+  function handleSubmit() {}
+  return (
+    <Container>
+      <Form
+        initialData={{ name: 'Humberto', email: 'hvcarvalhobrt@gmail.com' }}
+        onSubmit={handleSubmit}
+      >
+        <Input name="title" placeholder="Título do Meetup" />
+
+        {/* TODO Verificar por que não está expandindo */}
+        <Input
+          name="description"
+          multiline
+          rows={50}
+          placeholder="Descrição completa"
+        />
+
+        <hr />
+
+        <Input name="date" type="password" placeholder="Data do Meetup" />
+        <Input
+          name="location"
+          type="password"
+          placeholder="Localização do Meetup"
+        />
+
+        <Botao>
+          <button type="submit">
+            <MdAddCircleOutline color="#fff" size={22} />
+            <p>Salvar Meetup</p>
+          </button>
+        </Botao>
+      </Form>
+    </Container>
+  );
 }
--- src/pages/Meetup/styles.js
+++ src/pages/Meetup/styles.js
@@ -1,3 +1,89 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    textarea {
+      height: 500px;
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    span {
+      color: red;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+  }
+`;
+
+export const Botao = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
+
+    margin: 5px 0 0;
+    width: 180px;
+    height: 44px;
+
+    background: #f94d6a;
+
+    border: 0;
+    border-radius: 4px;
+
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.05, '#f94d6a')};
+    }
+
+    p {
+      font-size: 16px;
+      font-weight: bold;
+      color: #fff;
+    }
+  }
+`;
