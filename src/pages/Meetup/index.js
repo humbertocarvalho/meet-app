4e86aac7e625535abc4a8b4c738851eb2a7a0ea7
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-08 15:49:10 -0300

    Created Meetup page: to create and to edit meetups.

--- src/pages/Meetup/index.js
+++ src/pages/Meetup/index.js
@@ -1,51 +1,7 @@
 import React from 'react';
-import {
-  MdDeleteForever,
-  MdModeEdit,
-  MdLocationOn,
-  MdEvent,
-} from 'react-icons/md';
-import detail from '~/assets/detail.png';
 
-import { Container, Details } from './styles';
+// import { Container } from './styles';
 
 export default function Meetup() {
-  return (
-    <Container>
-      <header>
-        <h1>Meetup de React Native</h1>
-        <nav>
-          <button type="button" className="edit">
-            <MdModeEdit size={22} color="#ffff" />
-            <span>Editar</span>
-          </button>
-          <button type="button" className="cancel">
-            <MdDeleteForever size={22} color="#ffff" />
-            <span>Cancelar</span>
-          </button>
-        </nav>
-      </header>
-      <Details>
-        <img src={detail} alt="imagem" />
-
-        <p>
-          O Meetup de React Native é um evento que reúne a comunidade de
-          desenvolvimento mobile utilizando React a fim de compartilhar
-          conhecimento. Todos são convidados.
-          <br />
-          <br />
-          <br />
-          Caso queira participar como palestrante do meetup envie um e-mail para
-          organizacao@meetuprn.com.br
-        </p>
-
-        <footer>
-          <MdEvent size={20} />
-          24 de Junho, às 20h
-          <MdLocationOn size={20} />
-          Rua Guilherme Gembala, 260
-        </footer>
-      </Details>
-    </Container>
-  );
+  return <h1>Aqui é o create caralho vuai</h1>;
 }
---
+++ src/pages/MeetupDetail/styles.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 900px;
+  margin: 50px auto;
+
+  header {
+    width: 100%;
+    display: flex;
+
+    justify-content: space-between;
+
+    h1 {
+      color: #fff;
+      font-size: 32px;
+      font-weight: bold;
+    }
+    nav {
+      display: flex;
+      align-items: center;
+
+      button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 42px;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+
+        border-radius: 4px;
+
+        transition: background 0.3s;
+
+        &.edit {
+          width: 116px;
+          margin-right: 15px;
+          background: #4dbaf9;
+        }
+
+        &.cancel {
+          width: 132px;
+          background: #d44059;
+        }
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+`;
+
+export const Details = styled.div`
+  margin-top: 38px;
+  img {
+    width: 900px;
+    height: 300px;
+    display: block;
+  }
+
+  p {
+    margin-top: 38px;
+    font-size: 18px;
+    color: #fff;
+  }
+
+  footer {
+    display: flex;
+    align-items: center;
+
+    margin-top: 30px;
+    font-size: 16px;
+    color: #fff;
+    opacity: 0.6;
+
+    svg {
+      margin-left: 40px;
+      margin-right: 10px;
+
+      &:first-child {
+        margin-left: 0px;
+      }
+    }
+  }
+`;
