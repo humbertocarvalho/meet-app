21dd303ca8504caac3bea815e1956f58bbc5345d
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-11 18:39:46 -0300

    Add overflow-y: auto.

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #402845, #22202c);
+  min-height: 100%;
+
+  overflow-y: auto;
 `;
